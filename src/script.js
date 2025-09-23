e84538f566f1d093743540e5f379935e198e464b
BGNS42 <196232533+BGNS42@users.noreply.github.com>
2025-09-23 13:22:28 -0300

    create operate function receive an operator and two numbers. Calls the right math function for the operator

--- src/script.js
+++ src/script.js
@@ -1,5 +1,10 @@
+// variables
+let n1 = 0;
+let n2 = 0;
+let op = "";
 
 // Math functions
+
 function add(a, b) {
     return a + b;
 }
@@ -15,3 +20,24 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+function operate(op, n1, n2) {
+    switch (op) {
+        case "+":
+            return add(n1, n2);
+            break;
+        case "-":
+            return subtract(n1, n2);
+            break;
+        case "*":
+            return multiply(n1, n2);
+            break;
+        case "/":
+            return divide(n1, n2);
+            break;
+        default:
+            break;
+    };
+}
+;
+console.log(operate("/", 3, 2));
