91fc21e3e8927614f5d5e61080d01aedc825fa8b
BGNS42 <196232533+BGNS42@users.noreply.github.com>
2025-09-24 11:35:26 -0300

    calculator logic create an object to handle the operations. need to simplify

--- src/script.js
+++ src/script.js
@@ -1,12 +1,19 @@
 // variables
-let n1 = 0;
-let n2 = 0;
+let n1 = null;
+let n2 = null;
 let op = "";
+
+const buttons = document.querySelectorAll("button");
+
+let display = {
+    n1: "",
+    n2: "",
+};
 
 // Math functions
 
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a, b) {
@@ -39,5 +46,89 @@ function operate(op, n1, n2) {
             break;
     };
 }
-;
-console.log(operate("/", 3, 2));
+
+function checkInput(display, button){
+    if(button.textContent === "0"){
+        if(display.op === undefined){
+            if(display.n1 != "") {
+                display.n1 += button.textContent;
+            }
+        }
+        else {
+            if(display.n2 != "") {
+                display.n2 += button.textContent;
+            }
+        }
+    }
+    else {
+        if (display.op === undefined){
+        display.n1 = display.n1 + button.textContent;
+        }
+        else {
+            display.n2 = display.n2 + button.textContent;
+        }
+    }
+}
+
+// Listeners
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", () => {
+        console.log(button.classList);
+        const btnClass = button.classList[0];
+        const valor = button.textContent;
+        switch(btnClass) {
+            case "numbers":
+                checkInput(display, button);
+                break;
+            case "1":
+                if (display.op === undefined){
+                    display.n1 = display.n1 + button.textContent;
+                }
+                else {
+                    display.n2 = display.n2 + button.textContent;
+                }
+                break;
+            case "+":
+                if (display.op === undefined && display.n1 === "" && display.result) {
+                    display.n1 = display.result;
+                    display.op = button.textContent;
+                }
+                else if (display.op === undefined){
+                    display.op = button.textContent;
+                }
+                else {
+                    display.n1 = operate(display.op, display.n1, display.n2)
+                    display.n2 = "";
+                    display.op = button.textContent;
+                }
+                break;
+            case "-":
+                if (display.op === undefined){
+                    if(display.n1 == "") {
+                        display.n1 = display.result;
+                    }
+                    display.op = button.textContent;
+                }
+                else {
+                    display.n1 = operate(display.op, display.n1, display.n2)
+                    display.n2 = "";
+                    display.op = button.textContent;
+                }
+                break;
+            case "=":
+                display.result = operate(display.op, display.n1, display.n2)
+                display.n1 = "";
+                display.n2 = "";
+                display.op = undefined;
+                break;
+        }
+        console.log(display);
+    })
+});
+
+
+// Output Log
+
+//console.log(operate("+", 3, 5));
+console.log(display);
